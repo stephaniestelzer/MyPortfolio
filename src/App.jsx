74b3e696e59c795c63edb6c019bc58dd66032a77
Stephanie Stelzer <stephanie@stelzers.com>
2024-01-05 21:55:51 -0500

    Using Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { Navbar, Footer, StarsCanvas, Experience2 } from "./components";
+import { Navbar, Footer, Experience2 } from "./components";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
